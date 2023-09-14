9cb66c406a8bd99ea2716c7b47638bb840d2c15e
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-09-13 21:09:56 -0300

    add scatterplots

--- main.js
+++ main.js
@@ -21,14 +21,16 @@ var tooltip = d3
     const x = event.clientX;
     const y = event.clientY;
     const dataDate = event.target.getAttribute('data-date');
-    const dataGDP = event.target.getAttribute('data-gdp');
+    const dataTime = event.target.getAttribute('data-time');
+    const dataTimeMinutes = Math.floor(dataTime / 60);
+    const dataTimeSeconds = dataTime % 60;
   
     // Use the x and y coordinates to position the tooltip
-    tooltip.style('left', x-170+"px") // Adjust the offset as needed
-           .style('top', y-10+"px") // Adjust the offset as needed
+    tooltip.style('left', x+"px") // Adjust the offset as needed
+           .style('top', y+"px") // Adjust the offset as needed
            .style('opacity', 1)
            .attr('data-date', dataDate)
-           .html(`Date: ${dataDate}<br>GDP: $${dataGDP} Billion`);
+           .html(`Year: ${dataDate}<br>Time: ${dataTimeMinutes}:${dataTimeSeconds < 10 ? '0' : ''}${dataTimeSeconds}`);
   }
       
 // Function to hide the tooltip on mouseout
@@ -53,12 +55,12 @@ d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
 
     // Declare the x (horizontal position) scale.
     const x = d3.scaleLinear()
-        .domain([minX, maxX])
+        .domain([minX-1, maxX])
         .range([marginLeft, width - marginRight]);
 
         // Declare the y (vertical position) scale.
     const y = d3.scaleLinear()
-        .domain([minY, maxY])
+        .domain([minY-10, maxY])
         .range([height - marginBottom, marginTop]);
 
     // Add the x-axis.
@@ -78,6 +80,24 @@ d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
       return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
       })
     );
+
+    // Add the data points.
+    svg.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("class", "dot")
+        .attr("data-xvalue", d => d.Year)
+        .attr("data-yvalue", d => d.Seconds)
+        .attr("data-date", d => d.Year)
+        .attr("data-time", d => d.Seconds)
+        .attr("cx", d => x(d.Year))
+        .attr("cy", d => y(d.Seconds))
+        .attr("r", 6)
+        .attr("fill", d => d.Doping ? "red" : "green")
+        .on('mousemove', onMouseMove)
+        .on('mouseout', onMouseOut);
+
     
     // Append the SVG element.
     container.append(svg.node());
